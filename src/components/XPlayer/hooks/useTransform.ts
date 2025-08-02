bb182c8b5221609bd0398e9987374f58d6fff539
wrongrong <danronggm@gmail.com>
2025-08-02 22:15:09 +0800

    Revert "调整：旋转视频时完整显示画面"
    This reverts commit 2dff08c6ff30812dddecf764473bd915e9541084.

--- src/components/XPlayer/hooks/useTransform.ts
+++ src/components/XPlayer/hooks/useTransform.ts
@@ -26,7 +26,7 @@ function calculateScale(videoWidth: number, videoHeight: number, containerWidth:
 
   const scaleW = containerWidth / rotatedWidth
   const scaleH = containerHeight / rotatedHeight
-  return Math.min(scaleW, scaleH)
+  return ratio > 1 ? Math.min(scaleW, scaleH) : Math.max(scaleW, scaleH)
 }
 
 /** 画面转换 */
